a20d283f7f9e17d9da7cd957d171833b0d6f837e
mishika7608 <csds23040@glbitm.ac.in>
2025-12-17 17:17:59 +0530

    Ouput parsing of comma seperated values

--- nlp_foundation/LC_output_parsing.py
+++ nlp_foundation/LC_output_parsing.py
@@ -2,13 +2,38 @@ from dotenv import load_dotenv
 load_dotenv()
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage
- 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import CommaSeparatedListOutputParser
+from langchain_core.output_parsers import StrOutputParser
+from datetime import datetime
+
 llm = ChatGroq(
     model = "llama-3.1-8b-instant",
     temperature=0
 )
 
-response = llm.invoke([
-    HumanMessage(content="Ecplain LCEL in  one sentence")
+csv_parser = CommaSeparatedListOutputParser()
+
+csv_prompt = ChatPromptTemplate.from_messages([
+    ("system", "Return ONLY comma-seperated values."),
+    ("human", "List 5 cute bird names")
 ])
-print(response.content)
+
+chain = csv_prompt | llm | csv_parser
+csv_result = chain.invoke({})
+print("Parsed CSV:",csv_result)
+print("Type:",type(csv_result))
+
+date_parser = StrOutputParser()
+
+date_prompt = ChatPromptTemplate.from_messages([
+    ("system", "Return ONLY a date in ISO format (YYYY-MM-DD)."),
+    ("human", "What was habit tracking app 'Finch' published?")
+])
+
+chain = date_prompt | llm | date_parser
+date_result = chain.invoke({})
+
+parsed_date = datetime.fromisoformat(date_result)
+print("Parsed CSV:",parsed_date)
+print("Type:",type(parsed_date))
