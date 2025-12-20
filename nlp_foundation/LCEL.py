7b75e09b9d4607ea23c294ce5c117428716d2a1d
mishika7608 <csds23040@glbitm.ac.in>
2025-12-20 10:57:20 +0530

    Graphing runnables

--- nlp_foundation/LCEL.py
+++ nlp_foundation/LCEL.py
@@ -1,3 +1,36 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough #passes inputs through without alteration
+
+chat_template_tools = ChatPromptTemplate.from_template('''What are five most important tools a {job title} needs?
+                                         Answer only by listing the tools.
+                                         ''')
+chat_template_strategy = ChatPromptTemplate.from_template('''Considering tools provided, develop a strategy for effectively learning and mastering them: {tools}''')
+chat = ChatGroq(
+    model = "llama-3.1-8b-instant",
+    temperature=0.8,
+    max_tokens=100
+)
+
+string_parser = StrOutputParser()
+
+chain_long = chat_template_tools | chat | string_parser | {'tools':RunnablePassthrough()} | chat_template_strategy | chat | string_parser
+
+print(chain_long.get_graph().print_ascii())
+
+
+
+
+
+
+
+
+
+
+
 # RUNNABLE AND RUNNABLE SEQUENCE CLASSES
 # Prompt template, chat model, output parser are instances of runnable class
 #chatPrompt Template strats with runnable inherits invoke, betch, stream
@@ -86,30 +119,30 @@
 
 #PIPING
 
-from dotenv import load_dotenv
-load_dotenv()
-from langchain_groq import ChatGroq
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import CommaSeparatedListOutputParser
+# from dotenv import load_dotenv
+# load_dotenv()
+# from langchain_groq import ChatGroq
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import CommaSeparatedListOutputParser
 
-list_instructions = CommaSeparatedListOutputParser().get_format_instructions()
-list_instructions
-chat_template = ChatPromptTemplate.from_messages([('system', list_instructions),('human','I have recently adopted a {pet}. Could you suggest three {pet} names? \n')])
-print(chat_template.messages[0].prompt.template)
+# list_instructions = CommaSeparatedListOutputParser().get_format_instructions()
+# list_instructions
+# chat_template = ChatPromptTemplate.from_messages([('system', list_instructions),('human','I have recently adopted a {pet}. Could you suggest three {pet} names? \n')])
+# print(chat_template.messages[0].prompt.template)
 
-chat = ChatGroq(
-    model = "llama-3.1-8b-instant",
-    temperature=0.8
-)
+# chat = ChatGroq(
+#     model = "llama-3.1-8b-instant",
+#     temperature=0.8
+# )
 
 
-list_output_parser = CommaSeparatedListOutputParser()
-chat_template_result = chat_template.invoke({'pet':'penguin'})
-chat_result = chat.invoke(chat_template_result)
-print(list_output_parser.invoke(chat_result))
+# list_output_parser = CommaSeparatedListOutputParser()
+# chat_template_result = chat_template.invoke({'pet':'penguin'})
+# chat_result = chat.invoke(chat_template_result)
+# print(list_output_parser.invoke(chat_result))
 
-#  PIPE SYMBOL - links elements in expression language, output will be input for next component
+# #  PIPE SYMBOL - links elements in expression language, output will be input for next component
 
-chain = chat_template | chat | list_output_parser
+# chain = chat_template | chat | list_output_parser
 
-print(chain.invoke({'pet':'dog'}))
+# print(chain.invoke({'pet':'dog'}))
