b200bc05db4a1bfdc1959b2baee587600d0a6145
mishika7608 <csds23040@glbitm.ac.in>
2026-01-17 17:58:03 +0530

    Constructing webpage accepting user input

--- nlp_foundation/LLMengg.py
+++ nlp_foundation/LLMengg.py
@@ -1,21 +1,29 @@
 import streamlit as st
-st.title("This is a title text")
-st.title('_This_is :blue[a title] :speech_balloon:')
 
-#LaTeX
-st.title('$E = mc^2$')
+prompt = st.chat_input("Type your message", max_chars=50)
+if prompt: 
+    st.write(f"User message: {prompt}")
 
-st.header('This is a header')
-st.subheader('This is a sub header')
 
-st.text('This is plain text with no formatting')
 
-st.markdown('# This is a header\n **This is bold text** \n- This is a list item')
+#BASICS
+# st.title("This is a title text")
+# st.title('_This_is :blue[a title] :speech_balloon:')
 
-st.write('This is plain text using st.write')
+# #LaTeX
+# st.title('$E = mc^2$')
 
-data = {"Name": "Alice", "Age":30, "Occupation":"Engineer"}
-st.write(data)
+# st.header('This is a header')
+# st.subheader('This is a sub header')
+
+# st.text('This is plain text with no formatting')
+
+# st.markdown('# This is a header\n **This is bold text** \n- This is a list item')
+
+# st.write('This is plain text using st.write')
+
+# data = {"Name": "Alice", "Age":30, "Occupation":"Engineer"}
+# st.write(data)
 
 # write_stream - real time display of our data 
 # button state false until pressed - nested button(making nested button true outer button becomes false)
